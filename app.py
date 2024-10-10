a0031cef5fea878f4e1ab65d221ad4c0f86cab9c
MEOW <84795308+142CodeGreen@users.noreply.github.com>
2024-10-10 23:38:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ import os
 import gradio as gr
 import openai
 
-# import warnings
+import warnings
 # from langchain_core._api.deprecation import LangChainDeprecationWarning
 
-# warnings.filterwarnings("ignore", category=LangChainDeprecationWarning, module="llama_index")
+warnings.filterwarnings("ignore", category=LangChainDeprecationWarning, module="llama_index")
 
 # from langchain.memory import ChatMessageHistory  # Old import
 # from langchain_community.chat_message_histories import ChatMessageHistory
