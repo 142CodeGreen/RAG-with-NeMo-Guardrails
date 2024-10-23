ff9b20a3b1de6be52e1bcb631d104c22205e627d
MEOW <84795308+142CodeGreen@users.noreply.github.com>
2024-10-23 18:06:24 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -61,8 +61,7 @@ def load_documents(file_objs):
         #    output_fields=["field1","field2"]
         #)
 
-        vector_store = MilvusVectorStore(uri="./Config/kb/milvus_demo.db", dim=1024, overwrite=True,output_fields=[])
-        #vector_store = MilvusVectorStore(uri="./milvus_demo.db", dim=1024, overwrite=True,output_fields=[])
+        vector_store = MilvusVectorStore(uri="./milvus_demo.db", dim=1024, overwrite=True,output_fields=[])
         storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
         # Create the index from the documents
