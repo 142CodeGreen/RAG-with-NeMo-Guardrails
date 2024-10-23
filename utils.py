a8cfcc36c7854519bd4f4cd3e2d546610a185a7c
MEOW <84795308+142CodeGreen@users.noreply.github.com>
2024-10-23 17:56:40 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -60,8 +60,9 @@ def load_documents(file_objs):
         #    gpu_id=0,  # Specify the GPU ID to use
         #    output_fields=["field1","field2"]
         #)
-        
-        vector_store = MilvusVectorStore(uri="./milvus_demo.db", dim=1024, overwrite=True,output_fields=[])
+
+        vector_store = MilvusVectorStore(uri="./Config/kb/milvus_demo.db", dim=1024, overwrite=True,output_fields=[])
+        #vector_store = MilvusVectorStore(uri="./milvus_demo.db", dim=1024, overwrite=True,output_fields=[])
         storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
         # Create the index from the documents
