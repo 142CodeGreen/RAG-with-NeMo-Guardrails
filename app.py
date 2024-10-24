92b06d84633c0fcf19a99c43bef7402303c4fb89
MEOW <84795308+142CodeGreen@users.noreply.github.com>
2024-10-24 12:41:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from langchain_core._api.deprecation import LangChainDeprecationWarning
 warnings.filterwarnings("ignore", category=LangChainDeprecationWarning, module="llama_index")
 
 # Import from utils.py
-from utils import load_documents, index
+from utils import load_documents
 
 # Import actions 
 from Config.actions import init  # Import the init function
