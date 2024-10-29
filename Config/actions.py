ac9fb9bf7cea48d713218b1b9b053c6c6494454a
MEOW <84795308+142CodeGreen@users.noreply.github.com>
2024-10-29 11:13:29 +0800

    Update actions.py

--- Config/actions.py
+++ Config/actions.py
@@ -1,11 +1,11 @@
 from nemoguardrails.actions import action
 from nemoguardrails.actions.actions import ActionResult
 from llama_index.core import StorageContext, load_index_from_storage, PromptTemplate
-from nemoguardrails import LLMRails
+from nemoguardrails import LLMRails, KnowledgeBase
 
-import sys
-sys.path.append('../')  # Add the parent directory to the Python path
-from app import load_documents
+#import sys
+#sys.path.append('../')  # Add the parent directory to the Python path
+#from app import load_documents
 
 #def load_kb_from_storage():
 #    """Load the knowledge base from storage."""
@@ -27,14 +27,23 @@ def template(question, context):
     Answer in markdown:"""
 
 @action(is_system_action=True)
-def rag(context: dict, llm, kb) -> ActionResult:
+def rag(context: dict, llm, kb: KnowledgeBase) -> ActionResult:
     try:
         context_updates = {}
         message = context.get('last_user_message')
 
-        load_documents(file_objs)  
-        import app  # This import needs to be here to avoid circular imports
-        query_engine = app.query_engine #(similarity_top_k=20)
+        # Access the kb directory using kb.kb_dir
+        kb_dir = kb.kb_dir
+
+        documents = SimpleDirectoryReader(kb_dir).load_data()
+
+        # Create index and query_engine (adapt if needed)
+        vector_store = MilvusVectorStore(uri="./milvus_demo.db", dim=1024, overwrite=True, output_fields=[])
+        storage_context = StorageContext.from_defaults(vector_store=vector_store)
+        index = VectorStoreIndex.from_documents(documents, storage_context=storage_context)
+        query_engine   
+ = index.as_query_engine(similarity_top_k=20)   
+
         response = query_engine.query(message)
         relevant_chunks = response.source_nodes[0].node.text
 
